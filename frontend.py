dbe59883c370a9b8b4afdffbd8decd4420c67ece
CU Quants <88510904+CUQuants@users.noreply.github.com>
2021-09-02 10:19:57 -0600

    first version

---
+++ frontend.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import datetime as dt
+import yfinance as yf
+import streamlit as st
+
+from projects import *
+from inputType import *
+
+st.set_page_config(layout="wide")
+
+sidebar_options = ["security analysis", "projects and backend"]
+sidebar = st.sidebar.selectbox("pick an option", sidebar_options)
+
+if sidebar == "security analysis":
+    
+    st.title("Security Analysis")
+    
+    security_function_options = ["all", "general"]
+    security_function = st.sidebar.selectbox("security function", security_function_options)
+    
+    security = st.text_input("Please enter security here (seperate):")
+    input_type = inputType(security)
+        
+if sidebar == "projects and backend":
+
+    projects = Projects()
+    
+    st.title("Projects and backend")
+    st.write(projects.get_list())
+    
+    test = projects.get_prepared()
+    
+    
+'''
+    st.subheader("Codebase & Library: Not completed")
+    st.subheader("Codebase & Library: Partially Completed")
+    st.subheader("Codebase & Library: Completed")
+    st.subheader("Projects")
+    st.subheader("Backend")
+'''
+
---
+++ projects.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+class Projects():
+    
+    def __init__(self):
+    
+        self.codebase_url = 'https://docs.google.com/spreadsheets/d/1seBg9Gu5S4xDxUjEwduufSQWTA15V3N-IdpzB5PEOnk/edit#gid=0'
+        self.codebase_url = self.codebase_url.replace('/edit#gid=', '/export?format=csv&gid=')
+        self.codebase_library = pd.read_csv(self.codebase_url)
+        
+    def get_list(self):
+        return(self.codebase_library[["title", "area", "programming", "programming level", "math", "math level",
+                                      "finance / econ", "finance / econ level", "ideal majors"]])
+    
+    def get_prepared(self):
+        
+        for i in range(len(self.codebase_library)):
+            
+            row = self.codebase_library.iloc[i]
+            st.write(row[1], row[2], row[3], row[4])
+            break
+            
