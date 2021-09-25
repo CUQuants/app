d8c4150e835ce03914b3945e47e8927cfcbbfb23
Ryan Vandersmith <ryanvandersmith@gmail.com>
2021-09-25 15:55:57 -0600

    Overhaul and consolidate projects page

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CU Quants - Concept Demo App
 
 ### [Visit the app](https://share.streamlit.io/cuquants/app/main/frontend.py)
+
+### [Open the spreadsheet](https://docs.google.com/spreadsheets/d/1y-0I1ObMmBNRjSCXQHVt81bBOaz5_X6r0jRvElhtYxM/edit#gid=0)
--- app/page/projects/__init__.py
+++ app/page/projects/__init__.py
@@ -7,46 +7,50 @@ from app.utils import read_google_drive
 class ProjectsPage(Page):
     title = 'Projects and Backend'
 
-    codebase_library = read_google_drive('1seBg9Gu5S4xDxUjEwduufSQWTA15V3N-IdpzB5PEOnk')
-    open_source_project = read_google_drive('1F1QzElHO0dz4t8JPOEmEPDq13aa60Lbz9ildPm_KTnU')
-    packages = read_google_drive('1BVYK4qmgZaUJHw0x1Xq8G8EvuS-5-JIK009Ll1WeDNE')
+    sheet_id = '1y-0I1ObMmBNRjSCXQHVt81bBOaz5_X6r0jRvElhtYxM'
+
+    topics = read_google_drive(sheet_id, 0)
+    packages = read_google_drive(sheet_id, 329368905)
+    open_source_projects = read_google_drive(sheet_id, 985331080)
 
     def render(self):
-        self.render_lists()
+        self.render_topics()
+        st.markdown('---')
+        self.render_open_source_projects()
+        st.markdown('---')
+        self.render_packages()
 
-    def render_open_source_list(self):
+    def render_topics(self):
+        st.write(
+            "If you are interested in working on any of these projects please fill out our form [here](https://forms.gle/rPC4N9WmpjPJ5Kxa6)")
+        st.write(self.topics)
+
+    def render_open_source_projects(self):
+        st.header("Open-Source Projects")
         st.write(
             "If you are interested in working on any of these projects please fill out our form [here](https://forms.gle/oTh3nZJ4ffcd33KfA)")
 
-        for i in range(len(self.open_source_project)):
-            st.header(self.open_source_project[self.open_source_project.columns[0]][i])
-            st.write(self.open_source_project[self.open_source_project.columns[1]][i])
+        for i, row in self.open_source_projects.iterrows():
+            st.subheader(row['Name'])
+            st.write(row['Description'])
 
         st.write(
             'Backend Infrastructure list: [here](https://docs.google.com/spreadsheets/d/1falnY478mZZAY3lVGpDhpechekfTQvQK-F2DoxIvLyk/edit?usp=sharing)')
         st.write(
             'Bugs list: [here](https://docs.google.com/spreadsheets/d/1h0mNp1qTaz2XPqChcETDdqLye0v_oXdT1v5dsbdSTig/edit?usp=sharing)')
 
-    def render_packages_list(self):
-        st.title("Integration of Packages:")
+    def render_packages(self):
+        st.header("Integration of Packages")
         st.write(
             "If you are interested in working on any of these projects please fill out our form [here](https://forms.gle/9s5PyHr46ArMPuTG6)")
 
-        for i in range(len(self.packages)):
-            st.header(self.packages[self.packages.columns[0]][i])
-            st.write(self.packages[self.packages.columns[1]][i])
-            link = self.packages[self.packages.columns[2]][i]
-            st.write("see examples:", link)
-
-    def render_lists(self):
-        st.write(
-            "If you are interested in working on any of these projects please fill out our form [here](https://forms.gle/rPC4N9WmpjPJ5Kxa6)")
-        st.write(self.codebase_library)
-        self.render_open_source_list()
-        self.render_packages_list()
+        for i, row in self.packages.iterrows():
+            st.subheader(row['Name'])
+            st.write(row['Description'])
+            st.write(f"Examples [here]({row['Link']})")
 
     def render_prepared(self):
-        for i, row in self.codebase_library.iterrows():
+        for i, row in self.open_source_projects.iterrows():
             st.write("problem here")
             st.write(row[1], row[2], row[3], row[4])
             break
